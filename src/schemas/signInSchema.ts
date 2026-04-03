d035609f5444a57e136d432b8e06bc46e88a3647
Anup Padwalkar <padwalkaranup02@gmail.com>
2026-04-03 11:35:28 +0530

    Add database connection and user/message schemas; update dependencies

---
+++ src/schemas/signInSchema.ts
@@ -0,0 +1,7 @@
+import {z} from 'zod';
+
+export const signInSchema = z.object({
+    identifier:z.string(),
+    password: z.string().min(6,{message:"password must be of atleast 6 characters"})
+})
+

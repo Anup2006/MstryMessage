7b4d07c800763d77636fa752226dd5fe8bde12c6
Anup Padwalkar <padwalkaranup02@gmail.com>
2026-04-14 19:25:51 +0530

    feat: integrate next-auth for user authentication and add username uniqueness check

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,64 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request:Request){
+    await dbConnect()
+
+    try {
+        const {username,code}=await request.json()
+        const decodedUsername = decodeURIComponent(username)
+        const user=await UserModel.findOne({username:decodedUsername})
+
+        if(!user){
+            return Response.json(
+                {
+                    success:false,
+                    message:"User not found"
+                },
+                {status : 500}
+            )
+        }
+
+        const isCodeValid=user.verifyCode === code
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+
+        if(isCodeValid && isCodeNotExpired){
+            user.isVerified= true
+            await user.save()
+            return Response.json(
+                {
+                    success:true,
+                    message:"Account verified successfully"
+                },
+                {status : 500}
+            )
+        }else if(!isCodeNotExpired){
+            return Response.json(
+                {
+                    success:false,
+                    message:"verification code expired, please sign up agan to get new code "
+                },
+                {status : 400}
+            )
+        }else{
+            return Response.json(
+                {
+                    success:false,
+                    message:"Incoorect verification code"
+                },
+                {status : 500}
+            )
+        }
+
+
+    } catch (error) {
+        console.error("Error verifying User",error)
+        return Response.json(
+            {
+                success:false,
+                message:"Error verifying User"
+            },
+            {status : 500}
+        )
+    }
+}
